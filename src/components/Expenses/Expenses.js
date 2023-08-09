860bc4070d57d8b18fa6de6c29ffbbe49fc2f211
sameer798 <sameershams.adi@gmailcom>
2023-08-09 17:06:55 +0530

    adding conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,21 +13,28 @@ const filterChangeHandler = selectedYear =>{
   
 }
 
-const fiteredExpenses = props.item.filter(expense =>{
+const filteredExpenses = props.item.filter(expense =>{
  return expense.date.getFullYear().toString() === filteredYear;
-})
+});
+
+let expenseContent = <p>data not found</p>;
+if(filteredExpenses.length > 0){
+  expenseContent = filteredExpenses.map((expense)=>{
+    return <ExpenseItem
+       key = {expense.id}
+       date = {expense.date}
+       title = {expense.title}
+       amount = {expense.amount}
+     />
+   })
+}
+
 
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {fiteredExpenses.map((expense) => (
-        <ExpenseItem
-          key = {expense.id}
-          date={expense.date}
-          title={expense.title}
-          amount={expense.amount}
-        />
-      ))}
+      {expenseContent}
+     
       {/* <ExpenseItem title={props.item[0].title} date={props.item[0].date} amount={props.item[0].amount}></ExpenseItem>
         <ExpenseItem title={props.item[1].title} date={props.item[1].date} amount={props.item[1].amount}></ExpenseItem>
         <ExpenseItem title={props.item[2].title} date={props.item[2].date} amount={props.item[2].amount}></ExpenseItem>
